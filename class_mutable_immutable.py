ea64cc232dce100872c783d47eb806062345576a
july000 <3636398752@qq.com>
2023-02-28 00:04:13 +0800

    1. @property (setter) 2. time assign is vaild

--- class_mutable_immutable.py
+++ class_mutable_immutable.py
@@ -4,6 +4,9 @@ https://stackoverflow.com/questions/62029673/is-this-python-class-mutable
 * use _ at the beginning of an attribute name indicating that it's not supposed to be accessed and/or modified by any code outside of the class.
 * To access my_clock.time the interpreter needs to make some internal lookup of an attribute with the name time in a symbol table (just a fancy name for a dictionary) corresponding to the my_time object. 
 * At the same time, when you are accessing the local variable a, a similar lookup is being performed in another symbol table (aka dictionary) corresponding to the current local scope.
+
+@property Decorator 
+https://www.freecodecamp.org/news/python-property-decorator/
 '''
 
 class Clock:
@@ -13,6 +16,9 @@ class Clock:
     def time(self):
         return self._time
     
+    @time.setter        # ==setter
+    def time(self, value):
+        self._time = value
 
     def print_time(self):
         print(self._time)
@@ -20,6 +26,6 @@ class Clock:
 
 boston_clock = Clock('5:30')
 paris_clock = boston_clock  # object reference, same object, one object
-paris_clock.time = '10:30'  # .time equal a time attribute, access value; change value is invaild, error
+paris_clock.time = '10:30'  # update time
 boston_clock.print_time()   # result is 5:30
 print(id(paris_clock), id(boston_clock))    #same id

53fa67837282d8f010d0ee5a279fed17bc33c453
july000 <3636398752@qq.com>
2024-04-25 17:46:33 +0800

    refactor

--- Python/BinaryTree/Traverse/traverse.py
+++ Python/BinaryTree/Traverse/traverse.py
@@ -1,9 +1,118 @@
 
-class nonRecursiveTraverse:
-  def __init__(self, root):
-    self.root = root
+from collections import deque
+from baseClass import BinaryTree
+class Traverse(BinaryTree):
+  def __init__(self):
+    super().__init__()
+  
+  def preorderTraverse(self):
+    def process(root):
+      if not root:
+        return
+      preorderTraverseList.append(root.val)
+      process(root.left)
+      process(root.right)
+    preorderTraverseList = []
+    process(self.root)
+    return preorderTraverseList
+  
+  def inorderTraverse(self):
+    def process(root):
+      if not root:
+        return
+      process(root.left)
+      inorderTraverseList.append(root.val)
+      process(root.right)
+    inorderTraverseList = []
+    process(self.root)
+    return inorderTraverseList
 
-  def preorder(self):
+  def postorderTraverse(self):
+    def process(root):
+      if not root:
+        return
+      process(root.left)
+      process(root.right)
+      postorderTraverseList.append(root.val)
+    postorderTraverseList = []
+    process(self.root)
+    return postorderTraverseList
+
+  def levelorderTraversal(self):
+    levelorderList = []
+    if not self.root:
+      return levelorderList
+    dequeList = deque()
+    dequeList.append(self.root)
+    while dequeList:
+      cur = dequeList.popleft()
+      levelorderList.append(cur.val)
+      if cur.left:
+        dequeList.append(cur.left)
+      if cur.right:
+        dequeList.append(cur.right)
+    return levelorderList
+
+  def levelorderTraversal2(self):
+    levelorderList = []
+    if not self.root:
+      return levelorderList
+    dequeList = deque()
+    dequeList.append(self.root)
+    while dequeList:
+      size = len(dequeList)
+      levelNode = []
+      for i in range(size):
+        cur = dequeList.popleft()
+        levelNode.append(cur.val)
+        if cur.left:
+          dequeList.append(cur.left)
+        if cur.right:
+          dequeList.append(cur.right)
+      levelorderList.append(levelNode)
+    return levelorderList
+
+  def getWidthTree(self):
+    if not self.root:
+      return 0
+    q = deque()
+    nodeMap = {}
+    q.append(self.root)
+    nodeMap[self.root] = 1
+    res = 0
+    curNodeLevel = 1
+    LevelNodesNum = 0
+    while q:
+      cur = q.popleft()
+      if curNodeLevel != nodeMap[cur]:
+        res = max(res, LevelNodesNum)
+        LevelNodesNum = 1
+        curNodeLevel += 1
+      else:
+        LevelNodesNum += 1
+      if cur.left:
+        q.append(cur.left)
+        nodeMap[cur.left] = curNodeLevel+1
+      if cur.right:
+        q.append(cur.right)
+        nodeMap[cur.right] = curNodeLevel+1
+    return res
+
+
+  def getNodesNum(self, root):
+    if not root:
+      return 0
+    return self.getNodesNum(root.left) + self.getNodesNum(root.right) + 1
+
+  def getAllLeafs(self):
+    pass
+
+  def getDepthTree(self, root):
+    if not root:
+      return 0
+    return max(self.getDepthTree(root.left), self.getDepthTree(root.right)) + 1
+
+  def nonRecursivePreorder(self):
     stack = []
     preorderList = []
     if not self.root:
@@ -18,7 +127,7 @@ class nonRecursiveTraverse:
         stack.append(cur.left)
     return preorderList
   
-  def inorder(self):
+  def nonRecursiveInorder(self):
     stack = []
     inorderList = []
     if not self.root:
@@ -37,7 +146,7 @@ class nonRecursiveTraverse:
         break
     return inorderList
 
-  def postorder(self):
+  def nonRecursivePostorder(self):
     stack1 = []
     stack2 = []
     postList = []
